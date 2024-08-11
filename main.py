73527b25353da83051f6b7782600e7276db2d6bf
Carlos Hernandez <162843724+carlosh2004@users.noreply.github.com>
2024-08-11 17:19:10 -0500

    feat: add function barcode_reader

--- main.py
+++ main.py
@@ -1,16 +1,41 @@
-# This is a sample Python script.
-
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# Importing library
+import cv2
+from pyzbar.pyzbar import decode
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# Make one method to decode the barcode
+def barcode_reader(image):
+    # read the image in numpy array using cv2
+    img = cv2.imread(image)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # Decode the barcode image
+    detectedBarcodes = decode(img)
+
+    # If not detected then print the message
+    if not detectedBarcodes:
+        print("Barcode Not Detected or your barcode is blank/corrupted!")
+    else:
+
+        # Traverse through all the detected barcodes in image
+        for barcode in detectedBarcodes:
+
+            # Locate the barcode position in image
+            (x, y, w, h) = barcode.rect
+
+            # Put the rectangle in image using
+            # cv2 to highlight the barcode
+            cv2.rectangle(img, (x - 10, y - 10), (x + w + 10, y + h + 10), (255, 200, 200), 2)
+
+            if barcode.data != "":
+                # Print the barcode data
+                print(barcode.data)
+                print(barcode.type)
+
+            # Display the image
+    cv2.imshow("Image", img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    barcode_reader('image/1000309.tif')
